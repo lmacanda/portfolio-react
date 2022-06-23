3232381faa81ec42d72f5b49e833771c9ccbd529
lmacanda <laurina02@hotmail.com>
2022-06-23 18:02:57 +0100

    set active class in navbar

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -5,23 +5,46 @@ import { AiOutlineUser } from "react-icons/ai";
 import { MdOutlineWork } from "react-icons/md";
 import { BsBook } from "react-icons/bs";
 import { BsTelephone } from "react-icons/bs";
+import { useState } from "react";
 
 export default function Navbar() {
+  let [activeNav, setActiveNav] = useState("#");
+
   return (
     <nav>
-      <a href="/#">
+      <a
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+        href="/#"
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <MdOutlineWork />
       </a>
-      <a href="#portfolio">
+      <a
+        href="#portfolio"
+        onClick={() => setActiveNav("#portfolio")}
+        className={activeNav === "#portfolio" ? "active" : ""}
+      >
         <BsBook />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <BsTelephone />
       </a>
     </nav>
