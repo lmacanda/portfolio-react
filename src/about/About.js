51e0addb3e089fde0b1067b3eb9afd0fec07ac89
lmacanda <laurina02@hotmail.com>
2022-06-23 19:22:23 +0100

    set about css

--- src/about/About.js
+++ src/about/About.js
@@ -16,11 +16,15 @@ export default function About() {
         </div>
 
         <div className="about__content">
-          <div className="about__cards">
-            <article className="about_card">
-              <p>I'm Laura...</p>
-            </article>
-          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum
+          </p>
         </div>
       </div>
     </section>
