08641c9f758978f0c39dfe46ef2f1770ea0d95e9
lmacanda <laurina02@hotmail.com>
2022-06-24 16:22:54 +0100

    create portfolio section

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,10 +1,82 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from "./../files/app.JPG";
 
 export default function Portfolio() {
   return (
-    <section id="portfolio" className="portfolio">
-      <p>Portfolio</p>
+    <section id="portfolio">
+      <h5>My work</h5>
+      <h2>Porfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="app" />
+          </div>
+          <h3>portfolio item title</h3>
+          <a href="https://github.com/lmacanda" className="btn" target="blank">
+            Github
+          </a>
+          <a
+            href="https://spiffy-starship-85e67d.netlify.app/"
+            className="btn btn-primary"
+            target="blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="app" />
+          </div>
+          <h3>portfolio item title</h3>
+          <a href="https://github.com/lmacanda" className="btn" target="blank">
+            Github
+          </a>
+          <a
+            href="https://spiffy-starship-85e67d.netlify.app/"
+            className="btn btn-primary"
+            target="blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="app" />
+          </div>
+          <h3>portfolio item title</h3>
+          <a href="https://github.com/lmacanda" className="btn" target="blank">
+            Github
+          </a>
+          <a
+            href="https://spiffy-starship-85e67d.netlify.app/"
+            className="btn btn-primary"
+            target="blank"
+          >
+            Live Demo
+          </a>
+        </article>
+
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="app" />
+          </div>
+          <h3>portfolio item title</h3>
+          <a href="https://github.com/lmacanda" className="btn" target="blank">
+            Github
+          </a>
+          <a
+            href="https://spiffy-starship-85e67d.netlify.app/"
+            className="btn btn-primary"
+            target="blank"
+          >
+            Live Demo
+          </a>
+        </article>
+      </div>
     </section>
   );
 }
