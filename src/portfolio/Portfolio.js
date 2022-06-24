72fd397fc94e2fc0cd5ac996284d05982367c433
lmacanda <laurina02@hotmail.com>
2022-06-24 17:20:03 +0100

    generate portfolio items from array

--- src/portfolio/Portfolio.js
+++ src/portfolio/Portfolio.js
@@ -1,6 +1,32 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "./../files/app.JPG";
+import IMG1 from "./../files/img1.jpg";
+import IMG2 from "./../files/img2.jpg";
+import IMG3 from "./../files/img3.jpg";
+
+let data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "crypto",
+    github: "https://github.com/lmacanda",
+    demo: "https://spiffy-starship-85e67d.netlify.app/",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "charts",
+    github: "https://github.com/lmacanda",
+    demo: "https://spiffy-starship-85e67d.netlify.app/",
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: "app",
+    github: "https://github.com/lmacanda",
+    demo: "https://spiffy-starship-85e67d.netlify.app/",
+  },
+];
 
 export default function Portfolio() {
   return (
@@ -9,97 +35,24 @@ export default function Portfolio() {
       <h2>Porfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="app" />
-          </div>
-          <h3>portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/lmacanda"
-              className="btn"
-              target="blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://spiffy-starship-85e67d.netlify.app/"
-              className="btn btn-primary"
-              target="blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="app" />
-          </div>
-          <h3>portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/lmacanda"
-              className="btn"
-              target="blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://spiffy-starship-85e67d.netlify.app/"
-              className="btn btn-primary"
-              target="blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="app" />
-          </div>
-          <h3>portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/lmacanda"
-              className="btn"
-              target="blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://spiffy-starship-85e67d.netlify.app/"
-              className="btn btn-primary"
-              target="blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="app" />
-          </div>
-          <h3>portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/lmacanda"
-              className="btn"
-              target="blank"
-            >
-              Github
-            </a>
-            <a
-              href="https://spiffy-starship-85e67d.netlify.app/"
-              className="btn btn-primary"
-              target="blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>portfolio item title</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
