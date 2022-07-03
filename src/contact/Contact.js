67ee119482aa19eb529c9dd3737776f402cf23c7
lmacanda <laurina02@hotmail.com>
2022-07-03 18:14:44 +0100

    create contact section

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./contact.css";
 import { AiOutlineMail } from "react-icons/ai";
-import { AiOutlineWhatsApp } from "react-icons/ai";
+import { AiOutlinePhone } from "react-icons/ai";
 
 export default function Contact() {
   return (
@@ -15,10 +15,34 @@ export default function Contact() {
           <AiOutlineMail />
           <h4>Email</h4>
           <h5>laura.pantano2@gmail.com</h5>
-          <a href="mailto:laura.pantano2@gmail.com">Send a message</a>
+          <a href="mailto:laura.pantano2@gmail.com" target="blank">
+            Send me an email
+          </a>
+        </article>
+        <article className="contact_option">
+          <AiOutlinePhone />
+          <h4>Telephone Number</h4>
+          <h5>+351 915061359</h5>
         </article>
         {/*END OF CONTACT OPTIONS */}
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button type="submit" className="btn btn-primary">
+            Send me a Message
+          </button>
+        </form>
       </div>
     </section>
   );
