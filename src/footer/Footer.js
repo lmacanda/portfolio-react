405709754b91abf43edfc95cb5532e53f92bef88
lmacanda <laurina02@hotmail.com>
2022-07-03 23:01:12 +0100

    change root colors

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -3,8 +3,8 @@ import "./footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">
+    <footer className="footer">
       <p>Footer</p>
-    </div>
+    </footer>
   );
 }
