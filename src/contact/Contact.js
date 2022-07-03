6a6daab0c6a4069b5dc7df626ee24f8f2dfe2f97
lmacanda <laurina02@hotmail.com>
2022-07-03 18:56:39 +0100

    set contact css

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -9,21 +9,22 @@ export default function Contact() {
       <h5>Get in Touch</h5>
       <h2>Contact Me</h2>
 
-      <div className="container contact__contaner">
-        <div className="contact__options"></div>
-        <article className="contact_option">
-          <AiOutlineMail />
-          <h4>Email</h4>
-          <h5>laura.pantano2@gmail.com</h5>
-          <a href="mailto:laura.pantano2@gmail.com" target="blank">
-            Send me an email
-          </a>
-        </article>
-        <article className="contact_option">
-          <AiOutlinePhone />
-          <h4>Telephone Number</h4>
-          <h5>+351 915061359</h5>
-        </article>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>laura.pantano2@gmail.com</h5>
+            <a href="mailto:laura.pantano2@gmail.com" target="blank">
+              Send Email
+            </a>
+          </article>
+          <article className="contact__option">
+            <AiOutlinePhone className="contact__option-icon" />
+            <h4>Telephone Number</h4>
+            <h5>+351 915061359</h5>
+          </article>
+        </div>
         {/*END OF CONTACT OPTIONS */}
         <form action="">
           <input
@@ -40,7 +41,7 @@ export default function Contact() {
             required
           />
           <button type="submit" className="btn btn-primary">
-            Send me a Message
+            Send Message
           </button>
         </form>
       </div>
