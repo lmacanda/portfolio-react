13f20a3f276aebb3d3f3abb59785f16b0fe4b98e
lmacanda <laurina02@hotmail.com>
2022-07-06 20:46:55 +0100

    create footer section + css

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -4,7 +4,38 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>Footer</p>
+      <ul className="permalinks">
+        <li>
+          <a href="/#" rel="noopener noreferrer">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__source">
+        <small>
+          <a
+            href="https://github.com/lmacanda/weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Code
+          </a>{" "}
+          by Laura Pantano
+        </small>
+      </div>
     </footer>
   );
 }
